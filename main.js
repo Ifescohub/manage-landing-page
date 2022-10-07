b2672cc0a9e55c1fdf4d3d00138217b690c7f1d8
Udeh Ifeanyi <ifescoguy@gmail.com>
2022-10-07 21:36:02 +0100

    second commit

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ navToggle.addEventListener("click", ()=>{
     primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true)
+    
     primaryNav.toggleAttribute("data-visible")
+    navToggle.toggleAttribute("data-opened")
     document.querySelector("header").toggleAttribute("data-opened")
     
 })
