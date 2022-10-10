4a5278035aa4696fa820ceec62157e73d9ea1835
Udeh Ifeanyi <ifescoguy@gmail.com>
2022-10-10 19:13:02 +0100

    add screenshots to readmi

--- main.js
+++ main.js
@@ -34,11 +34,6 @@ const swiper = new Swiper('.swiper', {
   }
 });
 
-// if($(".slider .slide").length == 1) {
-//   $('.swiper-wrapper').addClass( "disabled" );
-//   $('.swiper-pagination').addClass( "disabled" );
-// }
-
 
 // FORMATTING FORM INPUT 
 
